370220397da1f9511a6d68c732197edea9974ecc
IH-MethodBox <65013694+lzr2006@users.noreply.github.com>
2022-02-05 17:10:02 +0800

    Add files via upload

---
+++ Plug-In:Remind/Main.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.Panel_Top = New System.Windows.Forms.Panel()
+        Me.Label_WarnCap = New System.Windows.Forms.Label()
+        Me.PictureBox_Exit = New System.Windows.Forms.PictureBox()
+        Me.Label_SayHello = New System.Windows.Forms.Label()
+        Me.Label_Time = New System.Windows.Forms.Label()
+        Me.Timer_timeGetHandler = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox_Pb = New System.Windows.Forms.PictureBox()
+        Me.Panel_Top.SuspendLayout()
+        CType(Me.PictureBox_Exit, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox_Pb, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel_Top
+        '
+        Me.Panel_Top.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Panel_Top.Controls.Add(Me.Label_WarnCap)
+        Me.Panel_Top.Controls.Add(Me.PictureBox_Exit)
+        resources.ApplyResources(Me.Panel_Top, "Panel_Top")
+        Me.Panel_Top.Name = "Panel_Top"
+        '
+        'Label_WarnCap
+        '
+        resources.ApplyResources(Me.Label_WarnCap, "Label_WarnCap")
+        Me.Label_WarnCap.Name = "Label_WarnCap"
+        '
+        'PictureBox_Exit
+        '
+        resources.ApplyResources(Me.PictureBox_Exit, "PictureBox_Exit")
+        Me.PictureBox_Exit.Name = "PictureBox_Exit"
+        Me.PictureBox_Exit.TabStop = False
+        '
+        'Label_SayHello
+        '
+        resources.ApplyResources(Me.Label_SayHello, "Label_SayHello")
+        Me.Label_SayHello.Name = "Label_SayHello"
+        '
+        'Label_Time
+        '
+        resources.ApplyResources(Me.Label_Time, "Label_Time")
+        Me.Label_Time.ForeColor = System.Drawing.Color.PaleGreen
+        Me.Label_Time.Name = "Label_Time"
+        '
+        'Timer_timeGetHandler
+        '
+        Me.Timer_timeGetHandler.Enabled = True
+        Me.Timer_timeGetHandler.Interval = 1000
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'PictureBox_Pb
+        '
+        Me.PictureBox_Pb.BackColor = System.Drawing.Color.Azure
+        resources.ApplyResources(Me.PictureBox_Pb, "PictureBox_Pb")
+        Me.PictureBox_Pb.Name = "PictureBox_Pb"
+        Me.PictureBox_Pb.TabStop = False
+        '
+        'Main
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Azure
+        Me.Controls.Add(Me.PictureBox_Pb)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label_Time)
+        Me.Controls.Add(Me.Label_SayHello)
+        Me.Controls.Add(Me.Panel_Top)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Main"
+        Me.Opacity = 0.833R
+        Me.ShowInTaskbar = False
+        Me.TopMost = True
+        Me.Panel_Top.ResumeLayout(False)
+        Me.Panel_Top.PerformLayout()
+        CType(Me.PictureBox_Exit, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox_Pb, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel_Top As Panel
+    Friend WithEvents PictureBox_Exit As PictureBox
+    Friend WithEvents Label_SayHello As Label
+    Friend WithEvents Label_Time As Label
+    Friend WithEvents Timer_timeGetHandler As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label_WarnCap As Label
+    Friend WithEvents PictureBox_Pb As PictureBox
+End Class
---
+++ Plug-In:Remind/Main.vb
@@ -0,0 +1,48 @@
+﻿Public Class Main
+    Declare Function SendMessage Lib "user32" Alias "SendMessageA" (
+                                              ByVal hwnd As IntPtr,
+                                              ByVal wMsg As Integer,
+                                              ByVal wParam As Integer,
+                                              ByVal lParam As Integer) _
+                                              As Boolean
+    Declare Function ReleaseCapture Lib "user32" Alias "ReleaseCapture" () As Boolean
+    Const WM_SYSCOMMAND = &H112
+    Const SC_MOVE = &HF010&
+    Const HTCAPTION = 2
+    Dim K As Int64 = 1644049999
+    Private Sub Panel_Top_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel_Top.MouseDown
+        ReleaseCapture()
+        SendMessage(Me.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0)
+    End Sub
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim nt As String = DateTime.Now
+        Label_Time.Text = nt.Split(" ")(3)
+    End Sub
+
+    Private Sub PictureBox_Exit_Click(sender As Object, e As EventArgs) Handles PictureBox_Exit.Click
+        Environment.Exit(0)
+    End Sub
+
+    Private Sub Timer_timeGetHandler_Tick(sender As Object, e As EventArgs) Handles Timer_timeGetHandler.Tick
+        Dim nt As String = DateTime.Now
+        Label_Time.Text = "现在是：" + nt
+        Dim Unix As String = TimeToUnix(nt)
+        Label2.Text = "任务剩余时间：" + (K - (Convert.ToInt64(Unix)-28801)).ToString()
+    End Sub
+
+    Public Function TimeToUnix(ByVal dteDate As Date) As String
+        If dteDate.IsDaylightSavingTime = True Then
+            dteDate = DateAdd(DateInterval.Hour, -1, dteDate)
+        End If
+        TimeToUnix = DateDiff(DateInterval.Second, #1/1/1970#, dteDate)
+    End Function
+
+    Private Sub Label_SayHello_Click(sender As Object, e As EventArgs) Handles Label_SayHello.Click
+
+    End Sub
+
+    Private Sub Panel_Top_Paint(sender As Object, e As PaintEventArgs) Handles Panel_Top.Paint
+
+    End Sub
+End Class
